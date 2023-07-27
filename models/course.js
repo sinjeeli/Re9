66ecccf7ca2161aa314e0e76e9702dd66c140704
sinjeeli <sinjeeli@gmail.com>
2023-07-26 22:44:01 -0400

    working state

---
+++ models/course.js
@@ -0,0 +1,55 @@
+const {Sequelize, DataTypes} = require('sequelize');
+
+module.exports = (sequelize) => {
+    class Course extends Sequelize.Model {}
+    Course.init({
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'Please provide a value for "title"',
+                },
+                notEmpty: {
+                    msg: 'Please provide a value for "title"',
+                },
+            },
+        },
+        description: {
+            type: Sequelize.TEXT,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'Please provide a value for "description"',
+                },
+                notEmpty: {
+                    msg: 'Please provide a value for "description"',
+                },
+            },
+        },
+        estimatedTime: {
+            type: Sequelize.STRING,
+            allowNull: true,
+        },
+        materialsNeeded: {
+            type: Sequelize.STRING,
+            allowNull: true,
+        },
+    }, {sequelize});
+
+    Course.associate = (models) => {
+        Course.belongsTo(models.User, {
+            foreignKey: {
+                fieldName: 'userId',
+                allowNull: false,
+            },
+        });
+    }
+
+    return Course;
+};
